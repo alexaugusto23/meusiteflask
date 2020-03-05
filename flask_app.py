d6eb6aee78a4e36c163c7c9bd2e1a587d211a6b8
Alexsandro Augusto Ignacio <1901705@SALAS.AULAS>
2020-03-04 21:59:28 -0300

    teste icone

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='icone.png')
+
+
 @app.route('/')
 def hello_world():
     return '<h1>Meu primeiro Site no flask localmente!</h1>'
