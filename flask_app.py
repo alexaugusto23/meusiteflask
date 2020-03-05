dfe562ff92b93c0c52addf72ce339871fb7a466a
Alexsandro Augusto Ignacio <1901705@SALAS.AULAS>
2020-03-04 21:41:06 -0300

    Alteração templates 2

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_templates
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,5 +11,5 @@ def hello_world():
 
 @app.route('/inicio')
 def inicio():
-    return render_templates('index.html')
+    return render_template('index.html')
 
